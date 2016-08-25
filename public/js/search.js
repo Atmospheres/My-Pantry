f6c7d7c321496f054fcf2500625235bb230f4af0
Bradley Jakubowski <Bradley Jakubowski>
2016-08-24 20:39:50 -0500

    Passes information back to view, cannot append to DOM as I am trying to as I .push, it auto closes tags which results in formatting errors. May need to make 1 string and += to add the elements on, then the append should format correctly.

--- public/js/search.js
+++ public/js/search.js
@@ -10,24 +10,36 @@
 
       // make the AJAX request
       $.getJSON('http://api.yummly.com/v1/api/recipes?_app_id=3e5b7dbe&_app_key=1d681685a57dac07e6df0b1c0df38de6&q=ginger' + '&requirePictures=true', function (json) {
-          // do all this on success
-          var recipes = [],
-              $ul;
+        // do all this on success
+        var recipes = [],
+            $recipes;
 
-          $.each(json, function (key, val) {
-            if (key === "matches"){
-              //recipes.push('<li id="recipe">Brad</li>')
-              for (i = 0; i < val.length ; i++) {
-              recipes.push('<li class="recipeName">' + val[i].recipeName + '</li>');
+        $.each(json, function (key, val) {
+          if (key === "matches"){
+            //recipes.push('<li id="recipe">Brad</li>')
+            recipes.push('<div data-example-id="thunmbnail-with-custom-content" class="bs-example"');
+            for (i = 0; i < val.length ; i++) {
+              if (i%3 === 0){
+                recipes.push('<div class="row">');
+              }
+              recipes.push('<div class="col-sm-6 col-md-4">');
+              recipes.push('<div class="thumbnail">' + '<img src="'+ val[i].smallImageUrls[0] + '" alt="100%x200" data-holder-rendered="true" style="height: 200px; width: 100%; display: block;"/>');
+              recipes.push('<div class="caption">' + '<h3 class="caption-text">' + val[i].recipeName + '</h3>');
+              recipes.push('<p class="caption-text">' + val[i].sourceDisplayName + '</p>');
+              recipes.push('</div></div></div>');
+              if ((i+1)%3 === 0){
+                recipes.push('</div>');
               }
             }
-          });
-          if (recipes.length < 1) {
-            recipes.push('<li>No results for parameters, try again!</li>');
+            recipes.push('</div>');
           }
-          $ul = $('<ul />').appendTo('.recipes');
-          $ul.append(recipes);
         });
-        e.preventDefault();
+      if (recipes.length < 1) {
+      recipes.push('<p>No results for parameters, try again!</p>');
+      }
+      $recipes = $('<div />').appendTo('.recipes');
+      $recipes.append(recipes);
     });
+    e.preventDefault();
+  });
 }(jQuery));
