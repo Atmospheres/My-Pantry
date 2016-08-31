612c7736f43dcd81e08d953e6974b1af02962b7d
Bradley <BAJakubowski@gmail.com>
2016-08-31 17:02:27 -0500

    Figured out why I couldn't get a popover to load, it was trying to run before it existed.  So instead of executing it on document ready i need to do it on another event like .click

--- public/js/search.js
+++ public/js/search.js
@@ -58,7 +58,8 @@
               recipes.push('<div class="thumbnail">' + '<img src="'+ val[i].imageUrlsBySize[90] + '" alt="100%x200" data-holder-rendered="true" style="height: 300px; width: 100%; display: block;"/>');
               recipes.push('<div class="caption">' + '<h3 class="caption-text">' + val[i].recipeName + '</h3>');
               recipes.push('<p class="caption-text">' + val[i].sourceDisplayName + '</p>');
-              recipes.push('<p><a href="#" role="buton" class="btn btn-primary"> Details </a><a href="#" role="buton" class="btn btn-primary"> Favorite </a>');
+              recipes.push('<p><a href="#" id="details" data-toggle="popover" title="Popover Header" data-content="popover content"> Details </a>');
+              recipes.push('<a href="#" class="btn btn-primary"> Favorite </a></p>');
               recipes.push('</div></div></div>');
               if ((i+1)%3 === 0){
                 recipes.push('</div>');
